7c2b8a999be8d77ea3426226a80e8f115e58e421
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-09 13:10:45 -0800

    add DojosandninjasController

--- src/main/java/com/vcabading/dojosandninjas/controllers/DojosandninjasController.java
+++ src/main/java/com/vcabading/dojosandninjas/controllers/DojosandninjasController.java
@@ -2,15 +2,17 @@ package com.vcabading.dojosandninjas.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /////////////////////////////////////////////////////////////////
-//HOME CONTROLLER
+//	DOJOS AND NINJAS CONTROLLER
 /////////////////////////////////////////////////////////////////
 
 @Controller
-public class HomeController {
-	@GetMapping("/")
+@RequestMapping("/dojos")
+public class DojosandninjasController {
+	@GetMapping("")
 	public String index() {
-		return "redirect:/dojos";
+		return "index.jsp";
 	}
 }
