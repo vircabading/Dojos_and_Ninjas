ef4b8e01b7a7a0d14d9a17751117f1269d475396
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-09 13:27:12 -0800

    add dojosid.jsp

--- src/main/java/com/vcabading/dojosandninjas/controllers/DojosandninjasController.java
+++ src/main/java/com/vcabading/dojosandninjas/controllers/DojosandninjasController.java
@@ -2,17 +2,43 @@ package com.vcabading.dojosandninjas.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /////////////////////////////////////////////////////////////////
 //	DOJOS AND NINJAS CONTROLLER
 /////////////////////////////////////////////////////////////////
 
 @Controller
-@RequestMapping("/dojos")
 public class DojosandninjasController {
-	@GetMapping("")
+	@GetMapping("/dojos")
 	public String index() {
 		return "index.jsp";
 	}
+
+	@GetMapping("/ninjas/new")
+	public String ninjasNew() {
+		return "ninjasnew.jsp";
+	}
+	
+	//	//// CREATE /////////////////////////////////////////////////
+
+		
+	//	//// RETRIEVE ///////////////////////////////////////////////
+
+	@GetMapping("/dojos/new")
+	public String dojosNew() {
+			return "dojosnew.jsp";
+	}
+
+	@GetMapping("/dojos/{id}")
+	public String dojosId(@PathVariable("id") Long id) {
+		return "dojosid.jsp";
+	}
+		
+	//	//// UPDATE /////////////////////////////////////////////////
+
+	
+
+	//	//// DELETE /////////////////////////////////////////////////
+
 }
