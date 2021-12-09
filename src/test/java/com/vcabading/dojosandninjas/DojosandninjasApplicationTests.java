9411a9edaea81c3e92b4610227d6746f01cd3549
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-09 12:42:46 -0800

    initial commit

---
+++ src/test/java/com/vcabading/dojosandninjas/DojosandninjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcabading.dojosandninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojosandninjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
