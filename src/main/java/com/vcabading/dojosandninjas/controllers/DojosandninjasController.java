df73ec1f2fc0b8cf2e8eb28b47d4ca8f8acde1f8
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-09 23:20:52 -0800

    add /dojos/new post route

--- src/main/java/com/vcabading/dojosandninjas/controllers/DojosandninjasController.java
+++ src/main/java/com/vcabading/dojosandninjas/controllers/DojosandninjasController.java
@@ -2,12 +2,16 @@ package com.vcabading.dojosandninjas.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.vcabading.dojosandninjas.models.Dojo;
 import com.vcabading.dojosandninjas.services.DojoService;
@@ -19,26 +23,43 @@ import com.vcabading.dojosandninjas.services.NinjaService;
 
 @Controller
 public class DojosandninjasController {
-	
-	//	//// FIELDS /////////////////////////////////////////////////
-	
+
+	// //// FIELDS /////////////////////////////////////////////////
+
 	@Autowired
 	DojoService dojoServ;
-	
+
 	@Autowired
 	NinjaService ninjaServ;
-	
-	//	//// SHOW ///////////////////////////////////////////////////
-	
+
+	// //// SHOW ///////////////////////////////////////////////////
+
 	@GetMapping("/ninjas/new")
 	public String ninjasNew() {
 		return "ninjasnew.jsp";
 	}
-	
-	//	//// CREATE /////////////////////////////////////////////////
 
-		
-	//	//// RETRIEVE ///////////////////////////////////////////////
+	@GetMapping("/dojos/new")
+	public String dojosNew(@ModelAttribute("dojo") Dojo dojo) {
+		return "dojosnew.jsp";
+	}
+
+	// //// CREATE /////////////////////////////////////////////////
+
+	@PostMapping("/dojos/new")
+	public String dojosNewPost(@Valid @ModelAttribute("dojo") Dojo dojo,
+				BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			List<Dojo> dojosList = this.dojoServ.retrieveAll();
+			model.addAttribute("dojosList",dojosList);
+			return "dojosnew.jsp";
+		} else {
+			this.dojoServ.create(dojo);
+			return "redirect:/dojos";
+		}
+    }
+
+	// //// RETRIEVE ///////////////////////////////////////////////
 
 	@GetMapping("/dojos")
 	public String index(Model model) {
@@ -46,21 +67,14 @@ public class DojosandninjasController {
 		model.addAttribute("dojosList", dojosList);
 		return "index.jsp";
 	}
-	
-	@GetMapping("/dojos/new")
-	public String dojosNew(@ModelAttribute("dojo") Dojo dojo) {
-			return "dojosnew.jsp";
-	}
 
 	@GetMapping("/dojos/{id}")
 	public String dojosId(@PathVariable("id") Long id) {
 		return "dojosid.jsp";
 	}
-		
-	//	//// UPDATE /////////////////////////////////////////////////
 
-	
+	// //// UPDATE /////////////////////////////////////////////////
 
-	//	//// DELETE /////////////////////////////////////////////////
+	// //// DELETE /////////////////////////////////////////////////
 
 }
